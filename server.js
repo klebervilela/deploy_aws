1af51508e482904e4b39ca25043354cc965dd32d
klebervilela <164577885+klebervilela@users.noreply.github.com>
2024-03-22 22:12:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-////import http from "http";
-//import app from "./src/app.js"
-
-//const PORT = 3000;
-
-////const server = http.createServer((req, res) => {
-//// res.writeHead(200, { "Content-Type": "text/plain" });
-////res.end(rotas[req.url]);
-////});
-
-//app.listen(PORT, () => {
-//  console.log("servidor escutando!");
-//});
-
-
-import http from "http";
+//import http from "http";
+import app from "./src/app.js"
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end();
-});
+//const server = http.createServer((req, res) => {
+// res.writeHead(200, { "Content-Type": "text/plain" });
+//res.end(rotas[req.url]);
+//});
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
+
+
+//import http from "http";
+
+//const PORT = 3000;
+
+//const server = http.createServer((req, res) => {
+//  res.writeHead(200, { "Content-Type": "application/json" });
+//  res.end();
+//});
+
+//server.listen(PORT, () => {
+//  console.log("servidor escutando!");
+//});
